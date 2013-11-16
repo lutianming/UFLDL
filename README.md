3f214308062b74e589f18ffbf5f62c6aa9a64045
Tianming.Lu <lutianming1005@gmail.com>
2013-11-16 16:16:02 +0100

    init, already finished 4 exercises

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 UFLDL
 =====
 
-my implementation of the exercise of UFLDF 
+my implementation of the exercise of [UFLDF](http://ufldl.stanford.edu/wiki/index.php/UFLDL_Tutorial)
---
+++ mnist/mnist.m
@@ -0,0 +1,44 @@
+visibleSize = 28*28;
+hiddenSize = 196;
+sparsityParam = 0.1;
+lambda = 3e-3;
+beta = 3;
+
+% Change the filenames if you've saved the files under different names
+% On some platforms, the files might be saved as 
+% train-images.idx3-ubyte / train-labels.idx1-ubyte
+images = loadMNISTImages('train-images-idx3-ubyte');
+labels = loadMNISTLabels('train-labels-idx1-ubyte');
+ 
+% We are using display_network from the autoencoder code
+display_network(images(:,1:100)); % Show the first 100 images
+disp(labels(1:10));
+
+
+patches = images(:, 1:10000);
+theta = initializeParameters(hiddenSize, visibleSize);
+
+%  Use minFunc to minimize the function
+addpath minFunc/
+options.Method = 'lbfgs'; % Here, we use L-BFGS to optimize our cost
+                          % function. Generally, for minFunc to work, you
+                          % need a function pointer with two outputs: the
+                          % function value and the gradient. In our problem,
+                          % sparseAutoencoderCost.m satisfies this.
+options.maxIter = 400;	  % Maximum number of iterations of L-BFGS to run 
+options.display = 'on';
+
+
+[opttheta, cost] = minFunc( @(p) sparseAutoencoderCost(p, ...
+                                   visibleSize, hiddenSize, ...
+                                   lambda, sparsityParam, ...
+                                   beta, patches), ...
+                              theta, options);
+
+%%======================================================================
+%% STEP 5: Visualization 
+
+W1 = reshape(opttheta(1:hiddenSize*visibleSize), hiddenSize, visibleSize);
+display_network(W1', 12); 
+
+print -djpeg mnist_features.jpg   % save the visualization to a file 
